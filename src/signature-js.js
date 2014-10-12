f9e6829d194215ffd1ee8c0ad80a2d5ece1538b4
scottmotte <scott@scottmotte.com>
2014-10-12 12:00:32 -0700

    Ability to prompt for text

--- src/signature-js.js
+++ src/signature-js.js
@@ -3,11 +3,13 @@
   var SignatureJs = function() {
     this.script = this.CurrentlyExecutedScript();
     this.jafja = undefined;
+    this.state = undefined;
 
     return this;
   };
 
   SignatureJs.prototype.init = function() {
+    var _this = this;
     if (this.script) {
       this.script.className += " signature-js-script";
       this.script.id        = "signature-js-script-"+this.uuid;
@@ -18,15 +20,29 @@
 
       signature_document.init();
 
-      jafja.bind('signature_document.rendered', function(values) {
-        signature_signing.fabrics = values.fabrics;
-        signature_signing.multiplier = values.multiplier;
+      jafja.bind('signature_document.rendered', function(result) {
+        signature_signing.fabrics = result.fabrics;
+        signature_signing.multiplier = result.multiplier;
         signature_signing.init();
         signature_chrome.init(signature_document.document);
       });
 
       jafja.bind('signature_chrome.state.changed', function(result) {
         console.log('signature_chrome.state.changed', result);
+        _this.state = result.current;
+      });
+
+      jafja.bind('signature_document.fabric.clicked', function(result) {
+        console.log('signature_document.fabric.clicked', result);
+        if (_this.state === "text_mode") {
+          signature_chrome.promptText();
+        }
+      });
+
+      jafja.bind('signature_chrome.text', function(text) {
+        alert(text);
+        // var payload = {x:1,y:1,content:text,page_number:1}
+        //signature_document.AddTextElement(text);
       });
 
     } else {
